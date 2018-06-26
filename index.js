d6c9605a6ec4e8ff616c97ab6815c4d4097b34f6
Kerry <mrkerrywashington@icloud.com>
2018-06-26 10:15:08 -0700

    fix path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 exports.Masks = require(path.join(__dirname, 'masks.js'));
 exports.Packet = require(path.join(__dirname, 'packet.js'));
 
-exports.kissDefs	= require("./kissDefs.js");
-exports.Defs		= require("./Defs.js");
-exports.Utils		= require("./Utils.js");
-exports.kissTNC		= require("./kissTNC.js");
+exports.kissDefs	= require("./kissdefs.js");
+exports.Defs		= require("./defs.js");
+exports.Utils		= require("./utils.js");
+exports.kissTNC		= require("./kisstnc.js");
