2f7c0e9523860f626f848294538875b28466041e
Kerry <mrkerrywashington@icloud.com>
2018-06-25 23:27:28 -0700

    merge of es6 and master

--- old/index.js
+++
@@ -1,6 +0,0 @@
-exports.kissDefs	= require("./kissDefs.js");
-exports.Defs		= require("./Defs.js");
-exports.Utils		= require("./Utils.js");
-exports.kissTNC		= require("./kissTNC.js");
-exports.Packet		= require("./Packet.js");
-exports.Session		= require("./Session.js");
