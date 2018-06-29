5c6bc6c6c5ca1fa2c23b429a0bd83ea1201ebec4
Kerry <mrkerrywashington@icloud.com>
2018-06-29 09:22:36 -0700

    formatting

--- kisstnc.js
+++ kisstnc.js
@@ -27,12 +27,7 @@ var kissTNC = function(args) {
 		}
 	);
 	
-	this.__defineGetter__(
-		"serialPort",
-		function() {
-			return properties.serialPort;
-		}
-	);
+	this.__defineGetter__("serialPort",function() {return properties.serialPort;});
 	
 	this.__defineSetter__(
 		"baudRate",
@@ -43,22 +38,12 @@ var kissTNC = function(args) {
 		}
 	);
 	
-	this.__defineGetter__(
-		"baudRate",
-		function() {
-			return properties.baudRate;
-		}
-	);
+	this.__defineGetter__("baudRate",function() {return properties.baudRate;});
 
 	this.__defineSetter__(
 		"txDelay",
 		function(txDelay) {
-			if(	typeof txDelay != "number"
-				||
-				txDelay < 0
-				||
-				txDelay > 255
-			) {
+			if(	typeof txDelay != "number"||txDelay < 0||txDelay > 255) {
 				throw "kissTNC: Invalid txDelay";
 			}
 			properties.txDelay = txDelay / 10;
@@ -66,22 +51,12 @@ var kissTNC = function(args) {
 		}
 	);
 	
-	this.__defineGetter__(
-		"txDelay",
-		function() {
-			return properties.txDelay * 10;
-		}
-	);
+	this.__defineGetter__("txDelay",function() {return properties.txDelay * 10;});
 
 	this.__defineSetter__(
 		"persistence",
 		function(persistence) {
-			if(	typeof persistence != "number"
-				||
-				persistence < 0
-				||
-				persistence > 1
-			) {
+			if(	typeof persistence != "number"||persistence < 0||persistence > 1) {
 				throw "kissTNC: Invalid persistence";
 			}
 			properites.persistence = (persistence * 256) - 1;
@@ -89,22 +64,12 @@ var kissTNC = function(args) {
 		}
 	);
 	
-	this.__defineGetter__(
-		"persistence",
-		function() {
-			return (properties.persistence / 256) + 1;
-		}
-	);
+	this.__defineGetter__("persistence",function() {return (properties.persistence / 256) + 1;});
 
 	this.__defineSetter__(
 		"slotTime",
 		function(slotTime) {
-			if(	typeof slotTime != "number"
-				||
-				slotTime < 0
-				||
-				slotTime > 255
-			) {
+			if(	typeof slotTime != "number"||slotTime < 0||slotTime > 255) {
 				throw "kissTNC: Invalid slotTime";
 			}
 			properties.slotTime = slotTime / 10;
@@ -112,54 +77,33 @@ var kissTNC = function(args) {
 		}
 	);
 	
-	this.__defineGetter__(
-		"slotTime",
-		function() {
-			return properties.slotTime * 10;
-		}
-	);
+	this.__defineGetter__("slotTime",function() {return properties.slotTime * 10;});
 	
 	this.__defineSetter__(
 		"txTail",
 		function(txTail) {
-			if(	typeof txTail != "number"
-				||
-				txTail < 0
-				||
-				txTail > 255
-			)
+			if(	typeof txTail != "number"||txTail < 0||txTail > 255) {
 				throw "kissTNC: Invalid txTail";
+			}
 			properties.txTail = txTail / 10;
 			sendFrame(ax25.kissDefs.TXTAIL, [properties.txTail]);
 		}
 	);
 	
-	this.__defineGetter__(
-		"txTail",
-		function() {
-			return properties.txTail * 10;
-		}
-	);
+	this.__defineGetter__("txTail",function() {return properties.txTail * 10;});
 
 	this.__defineSetter__(
 		"fullDuplex",
 		function(fullDuplex) {
-			if(typeof fullDuplex != "boolean")
+			if(typeof fullDuplex != "boolean"){
 				throw "kissTNC: fullDuplex must be boolean";
+			}
 			properties.fullDuplex = fullDuplex;
-			sendFrame(
-				ax25.kissDefs.FULLDUPLEX,
-				[(properties.fullDuplex) ? 1 : 0]
-			);
+			sendFrame(ax25.kissDefs.FULLDUPLEX,[(properties.fullDuplex) ? 1 : 0]);
 		}
 	);
 	
-	this.__defineGetter__(
-		"fullDuplex",
-		function() {
-			return (properties.fullDuplex == 1) ? true : false;
-		}
-	);
+	this.__defineGetter__("fullDuplex",function() {return (properties.fullDuplex == 1) ? true : false;});
 	
 	this.serialPort		= args.serialPort;
 	this.baudRate		= args.baudRate;
